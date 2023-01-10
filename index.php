d07f144748ac4ef2c08ad99e80d70baee669102b
Davids Kokojevs <davids.kokojevs@gmail.com>
2023-01-10 16:31:43 +0200

    Complete homework

---
+++ index.php
@@ -0,0 +1,65 @@
+<?php
+//Classwork
+
+//  Task 1
+// $minutes = $argv[1];
+//
+// if ($minutes <= 15 && $minutes > 0){
+//     print_r("1");
+// } else if ($minutes <= 30 && $minutes > 15){
+//     print_r("2");
+// } else if ($minutes <= 45 && $minutes > 30){
+//       print_r("3");
+// } else {
+//       print_r("4");
+// }
+
+// Task 2
+// $a = 2;
+// $b = pow($a , 9);
+// $c = $b % 3;
+//
+// if($c === 0 ) {
+//     print_r($b / 3);
+// } else {
+//     print_r($b." Не кратен 3");
+// }
+
+
+// Homework
+
+//Task 1
+// $day = $argv[1];
+//
+// if ($day > 0 && $day <= 10){
+//     print_r("First Decad");
+// } else if ($day > 10 && $day <= 20) {
+//     print_r("Second Decad");
+// } else if ($day > 20 && $day <= 31) {
+//     print_r("Third Decad");
+// } else {
+//     print_r("Please write day from 1 till 31");
+// }
+
+// Task 2
+// $a = 3;
+// $b = pow($a, 6);
+//
+// echo (($b % 5) === 0) ? $b / 5 : $b." not multiple of 5";
+// print_r((($b % 5) === 0) ? $b / 5 : $b." not multiple of 5"); //Testing different methods of results
+
+// Task 3
+// -45 -42 -39 -36 -33 -30 -27 -24 -21 -18 -15 -12 -9 4 8 ... n+4 - Leap years
+// $year = -12;
+//
+// for ($a = -48; $a <= -9; $a = $a + 3) {
+//     if ( $a === $year ){
+//         print_r($year." - leap year");
+//     }
+// };
+// if ($year % 4 == 0 && $year > 0){
+//    print_r($year." - leap year");
+// } else if ($year !== -45 && $year !== -42 && $year !== -39 && $year !== -36 && $year !== -33 && $year !== -30 && $year !== -27 &&
+//            $year !== -24 && $year !== -21 && $year !== -18 && $year !== -15 && $year !== -12 && $year !== -9) {
+//     print_r($year. " - not a leap year");
+// }
